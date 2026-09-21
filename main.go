66f74d889c74397b9636bcb4c10c9cc04ff37603
lokyshka <golovanov.roma72@bk.ru>
2026-09-21 07:15:11 +0300

    созданы пару сцен регистрации.

--- main.go
+++ main.go
@@ -21,7 +21,117 @@ func maxtest(ctx maxbot.Context) error {
 }
 */
 
-var tgscn map[int64]int8 = make(map[int64]int8)
+var regions = [...]string{
+	// республики
+	"республика адыгея",
+	"республика алтай",
+	"республика башкортостан",
+	"республика бурятия",
+	"республика дагестан",
+	"донецкая народная республика",
+	"республика ингушетия",
+	"кабардино-балкарская республика",
+	"республика калмыкия",
+	"карачаево-черкесская республика",
+	"республика карелия",
+	"республика коми",
+	"республика крым",
+	"луганская народная республика",
+	"республика марий эл",
+	"республика мордовия",
+	"республика саха",
+	"республика северная осетия",
+	"республика татарстан",
+	"республика тыва",
+	"удмуртская республика",
+	"республика хакасия",
+	"чеченская республика",
+	"чувашская республика",
+
+	// края
+	"алтайский край",
+	"забайкальский край",
+	"камчатский край",
+	"краснодарский край",
+	"красноярский край",
+	"пермский край",
+	"приморский край",
+	"ставропольский край",
+	"хабаровский край",
+
+	// области
+	"амурская область",
+	"архангельская область",
+	"астраханская область",
+	"белгородская область",
+	"брянская область",
+	"владимирская область",
+	"волгоградская область",
+	"вологодская область",
+	"воронежская область",
+	"ивановская область",
+	"иркутская область",
+	"калининградская область",
+	"калужская область",
+	"кемеровская область",
+	"кировская область",
+	"костромская область",
+	"курганская область",
+	"курская область",
+	"ленинградская область",
+	"липецкая область",
+	"магаданская область",
+	"московская область",
+	"мурманская область",
+	"нижегородская область",
+	"новгородская область",
+	"новосибирская область",
+	"омская область",
+	"оренбургская область",
+	"орловская область",
+	"пензенская область",
+	"псковская область",
+	"ростовская область",
+	"рязанская область",
+	"самарская область",
+	"саратовская область",
+	"сахалинская область",
+	"свердловская область",
+	"смоленская область",
+	"тамбовская область",
+	"тверская область",
+	"томская область",
+	"тульская область",
+	"тюменская область",
+	"ульяновская область",
+	"челябинская область",
+	"ярославская область",
+	"запорожская область",
+	"херсонская область",
+
+	// города федерального значения
+	"москва",
+	"санкт-петербург",
+	"севастополь",
+
+	// автономная область
+	"еврейская автономная область",
+
+	// автономные округа
+	"ненецкий автономный округ",
+	"ханты-мансийский автономный округ",
+	"чукотский автономный округ",
+	"ямало-ненецкий автономный округ",
+}
+
+type user struct {
+	sc     int8
+	region int8
+	city   string
+	mu     sync.Mutex
+}
+
+var users = make(map[int64]*user)
 var tgmutex sync.RWMutex
 var tg *tele.Bot
 
@@ -29,12 +139,13 @@ const selrole int8 = 0
 const idle int8 = 1
 
 const clselreg int8 = 2
-const clselmun int8 = 3
+const clselcity int8 = 3
 const clsellib int8 = 4
-const claskbook int8 = 5
-const cledittimeget int8 = 6
-const cledittimeret int8 = 7
-const cllist int8 = 8
+const clafter int8 = 5
+const claskbook int8 = 6
+const cledittimeget int8 = 7
+const cledittimeret int8 = 8
+const cllist int8 = 9
 
 const libselreg int8 = -1
 const libselmul int8 = -2
@@ -46,7 +157,7 @@ func handletg(ctx tele.Context) {
 
 	userid := ctx.Sender().ID
 	tgmutex.RLock()
-	scene := tgscn[userid]
+	scene := users[userid].sc
 	tgmutex.RUnlock()
 
 	switch scene {
@@ -61,16 +172,45 @@ func handletg(ctx tele.Context) {
 
 		ctx.Send(welcomemsg, selector)
 		tg.Handle(&btncl, func(c tele.Context) error {
-			tgscn[userid] = clselreg
+			users[userid].sc = clselreg
+			handletg(ctx)
 			return nil
 		})
 		tg.Handle(&btncl, func(c tele.Context) error {
-			tgscn[userid] = libselreg
+			users[userid].sc = libselreg
+			handletg(ctx)
 			return nil
 		})
 
 	case clselreg:
+		ctx.Send("Введите название региона. Пример: \"Республика Татарстан\", \"Москва\".")
+		users[userid].sc = clselcity
 
+	case clselcity:
+		var flag bool
+		region := ctx.Message().Text
+
+		for _, item := range regions {
+			if item == region {
+				flag = true
+				break
+			}
+		}
+
+		if !flag {
+			ctx.Send("Неверное название региона! Попробуйте еще раз в другом формате.")
+			return
+		}
+
+		// привязываем человека к региону
+
+		ctx.Send("Отлично! Теперь, введите город.")
+		users[userid].sc = clsellib
+
+	case clsellib:
+		// привязываем человека к городу
+
+		ctx.Send("Выберите библиотеку. Нету в списке? Возможно, библиотека не зарегистрирована в боте или вы неверно указали город.")
 	}
 
 }
@@ -108,8 +248,8 @@ func main() {
 		return
 	}
 
-	// запускаем бота max
 	/*
+		// запускаем бота max
 		max, err := maxbot.NewApi(maxtoken)
 		if err != nil {
 			log.Fatal(err)
@@ -119,17 +259,19 @@ func main() {
 	tg.Handle("/start", func(ctx tele.Context) error {
 		userid := ctx.Sender().ID
 		tgmutex.Lock()
-		tgscn[userid] = selrole
+		users[userid].sc = selrole
 		tgmutex.Unlock()
 		log.Println("new /start")
 
 		handletg(ctx)
 		return nil
 	})
-	/* max.Handle("/start", func(ctx maxbot.Context) error {
-		return nil
-	}) */
+	/*
+		max.Handle("/start", func(ctx maxbot.Context) error {
+			return nil
+		})
+		go max.Start()
+	*/
 
 	tg.Start()
-	// max.Start()
 }
